e894d51260222338439aa05a762c18bc55c322ce
Shivakumar Kirigeri <shivakumar641@gmail.com>
2026-06-07 08:31:22 +0530

    task:placeholder for alerts

--- src/repos/gets/getStatesAndUnions.js
+++ src/repos/gets/getStatesAndUnions.js
@@ -1,12 +1,14 @@
 const { connectDB } = require("../../database/connectDB");
 const pool = connectDB();
-
+const axios = require("axios");
+require("dotenv").config();
 const getStatesAndUnions = async () => {
   try {
     const result = await pool.query(
       `SELECT id, state_union_code, rto_code, state_union_name, is_union_territory from states_unions where
       is_active=true order by state_union_name;`,
     );
+    //test whatsapp
     return {
       statuscode: 200,
       successstatus: true,
